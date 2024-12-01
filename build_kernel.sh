ff09c2c20ff73963d3eaa8b307d08da6aece07ad
Rissu <90097027+rsuntk@users.noreply.github.com>
2024-12-01 16:01:55 +0700

    Adapt for CI

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,25 +1,22 @@
-#!/bin/bash
+#! /usr/bin/env bash
+
+# arg parser
+if [ $# -lt 3 ]; then
+	echo -e "Usage: `basename $0` <defconfig> <ksu:boolean> <job_count:int>"
+	exit
+else
+	DEFCONFIG="$1"
+	KSU="$2"
+	JOBS="$3"
+fi
 
 export ARCH=arm64
-
-if [ -z $CROSS_COMPILE ] || [ -z $PATH ]; then
-	export CROSS_COMPILE="/home/rissu/Documents/toolchains/aarch64-linux-android/bin/aarch64-linux-android-"
-	export PATH="/home/rissu/Documents/toolchains/clang-11/bin:$PATH"
-fi
-
-if [ -z $DEFCONFIG ]; then
-	DEFAULT_DEFCONFIG="vendor/rsuntk_defconfig"
-else
-	DEFAULT_DEFCONFIG=$DEFCONFIG
-fi
-
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 export PROJECT_NAME=a23
 
-# do not remove
+# main build script, don't remove it
 source ./scripts/rsubuild.sh
 
-# fmt <target> <jobs> <defconfig> <llvm> <llvm_ias> <extra_env>
-build defconfig $(nproc --all) `echo $DEFAULT_DEFCONFIG` false false `echo $KERNEL_MAKE_ENV`
-build kernel $(nproc --all) `echo $DEFAULT_DEFCONFIG` false false `echo $KERNEL_MAKE_ENV`
+build defconfig $(nproc --all) `echo $DEFCONFIG` false false `echo $KERNEL_MAKE_ENV`
+build kernel $(nproc --all) `echo $DEFCONFIG` `echo $KERNEL_MAKE_ENV`
